f95c8fb34816c3251da01db9da4855d1de980359
zeeforum <zartashzulfiqar@gmail.com>
2017-03-27 12:57:30 +0500

    Delete config.php

--- includes/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-	define('BASE_PATH', dirname(__DIR__));
-	define('TMP_PATH', BASE_PATH.'site-content/tmp/');
-	define('IMAGES_PATH', BASE_PATH.'site-content/images/');
-	define('IMAGES_TABLE_NAME', 'images');
-?>
